d72709ec501429c993f8d68c4c44e7917bf0088f
Len <40720638+destro174@users.noreply.github.com>
2022-10-19 18:15:23 +0200

    add runServer task

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,4 +1,6 @@
 import java.io.ByteArrayOutputStream
+import java.io.FileOutputStream
+import java.net.URL
 
 plugins {
     `maven-publish`
@@ -26,6 +28,26 @@ tasks {
         dependsOn(shadowJar)
     }
 
+    runServer {
+        val dir = File(System.getProperty("user.home") + "/share/devserver/")
+        if (!dir.parentFile.exists()) {
+            dir.parentFile.mkdirs()
+        }
+        runDirectory.set(dir)
+
+        val fileName = "/galaxy.jar"
+        val file = File(dir.path + fileName)
+
+        if (!file.parentFile.exists()) {
+            file.parentFile.mkdirs()
+        }
+        if (!file.exists()) {
+            download("https://repo.destro.xyz/snapshots/com/alttd/Galaxy-Server/Galaxy-paperclip-1.19.2-R0.1-SNAPSHOT-reobf.jar", file)
+        }
+        serverJar(file)
+        minecraftVersion("1.19.2")
+    }
+
 }
 
 bukkit {
@@ -44,4 +66,12 @@ fun gitCommit(): String {
         standardOutput = os
     }
     return String(os.toByteArray()).trim()
+}
+
+fun download(link: String, path: File) {
+    URL(link).openStream().use { input ->
+        FileOutputStream(path).use { output ->
+            input.copyTo(output)
+        }
+    }
 }
